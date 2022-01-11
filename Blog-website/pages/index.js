3d0efb300131b642a545f95a63fbc1af1065878b
martin kyalo <martinkkyalo10@gmail.com>
2022-01-11 04:12:41 +0300

    posts rendered to the home page using getStaticProps

--- Blog-website/pages/index.js
+++ Blog-website/pages/index.js
@@ -1,54 +1,22 @@
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page-components/FeaturedPosts";
 import Hero from "../components/home-page-components/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-export default function HomePage() {
-  const dummyPosts = [
-    {
-      slug: "getting-started-with-nextjs",
-      title: "Getting Started with Nextjs",
-      image: "i1.jpg",
-      excerpt:
-        "NextJS is a React framework for production. It makes building fullstack React web apps easy.",
-      date: "2022-02-10",
-    },
-    {
-      slug: "getting-started-with-nextjs2",
-      title: "Getting Started with Nextjs",
-      image: "i2.jpg",
-      excerpt:
-        "NextJS is a React framework for production. It makes building fullstack React web apps easy.",
-      date: "2022-02-10",
-    },
-    {
-      slug: "getting-started-with-nextjs3",
-      title: "Getting Started with Nextjs",
-      image: "i3.jpg",
-      excerpt:
-        "NextJS is a React framework for production. It makes building fullstack React web apps easy.",
-      date: "2022-02-10",
-    },
-    {
-      slug: "getting-started-with-nextjs4",
-      title: "Getting Started with Nextjs",
-      image: "i4.jpg",
-      excerpt:
-        "NextJS is a React framework for production. It makes building fullstack React web apps easy.",
-      date: "2022-02-10",
-    },
-    {
-      slug: "getting-started-with-nextjs5",
-      title: "Getting Started with Nextjs",
-      image: "i5.jpg",
-      excerpt:
-        "NextJS is a React framework for production. It makes building fullstack React web apps easy.",
-      date: "2022-02-10",
-    },
-  ];
+export default function HomePage(props) {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={dummyPosts} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
+}
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 300,
+  };
 }
